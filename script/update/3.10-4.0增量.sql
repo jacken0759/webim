2300967e69925e614fe6e67df498da87cae6434f
贝密 <open@ukewo.cn>
2018-08-29 17:50:32 +0800

    no commit message

--- script/update/3.10-4.0增量.sql
+++ script/update/3.10-4.0增量.sql
@@ -2,3 +2,5 @@ ALTER TABLE uk_chat_message ADD title varchar(255) COMMENT '标题';
 ALTER TABLE uk_chat_message ADD headimgurl varchar(255) COMMENT '缩略图';
 ALTER TABLE uk_chat_message ADD qusid varchar(32) COMMENT '问题ID';
 ALTER TABLE uk_chat_message ADD code varchar(32) COMMENT '代码';
+
+ALTER TABLE uk_chat_message ADD usetime datetime COMMENT '最后一次评价时间';
