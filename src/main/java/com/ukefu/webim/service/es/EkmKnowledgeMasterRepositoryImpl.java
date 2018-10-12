e2e6c759e5df498f803fe3b34b034d45ebea3dbb
羽丰乐成 <yunweixiang@ukewo.cn>
2018-10-12 14:04:28 +0800

    no commit message

--- src/main/java/com/ukefu/webim/service/es/EkmKnowledgeMasterRepositoryImpl.java
+++ src/main/java/com/ukefu/webim/service/es/EkmKnowledgeMasterRepositoryImpl.java
@@ -256,10 +256,9 @@ public class EkmKnowledgeMasterRepositoryImpl implements EkmKnowledgeMasterESRep
 	@SuppressWarnings("deprecation")
 	private Page<EkmKnowledgeMaster> processQuery(BoolQueryBuilder boolQueryBuilder, Pageable page){
 		
-		
+		//过滤掉已过期的知识
 		QueryBuilder beginFilter = QueryBuilders.boolQuery().should(QueryBuilders.missingQuery("begintime")).should(QueryBuilders.rangeQuery("begintime").to(new Date().getTime())) ;
 		QueryBuilder endFilter = QueryBuilders.boolQuery().should(QueryBuilders.missingQuery("endtime")).should(QueryBuilders.rangeQuery("endtime").from(new Date().getTime())) ;
-		
 		
 		NativeSearchQueryBuilder searchQueryBuilder = new NativeSearchQueryBuilder().withQuery(boolQueryBuilder).withFilter(QueryBuilders.boolQuery().must(beginFilter).must(endFilter)).withSort(new FieldSortBuilder("createtime").unmappedType("date").order(SortOrder.DESC));
 		searchQueryBuilder.withHighlightFields(new Field("title") , new Field("content")) ;
