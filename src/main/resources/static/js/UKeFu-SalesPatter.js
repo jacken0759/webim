1ce250db335109024ac6f4d4292b720e0e3743ce
贝密 <open@ukewo.cn>
2018-10-17 17:57:09 +0800

    no commit message

--- src/main/resources/static/js/UKeFu-SalesPatter.js
+++ src/main/resources/static/js/UKeFu-SalesPatter.js
@@ -2,12 +2,12 @@ jsPlumb.ready(function () {
 
     var j = window.j = jsPlumb.getInstance({Endpoint: ["Dot", {radius: 2}],
         Connector:"StateMachine",
-        HoverPaintStyle: {stroke: "#1e8151", strokeWidth: 2 },
+        HoverPaintStyle: {stroke: "#32c24d", strokeWidth: 2 },
         ConnectionOverlays: [
             [ "Arrow", {
                 location: 1,
                 id: "arrow",
-                length: 10,
+                length: 14,
                 foldback: 0.8
             } ]
         ],
@@ -19,33 +19,6 @@ jsPlumb.ready(function () {
         });
     });
 
-    var evts = document.querySelector("#events");
-    var _appendEvent = function(name, detail) {
-        evts.innerHTML = "<br/><strong>" + name + "</strong><br/> " + detail + "<br/>" + evts.innerHTML;
-    };
-    j.bind("group:addMember", function(p) {
-        _appendEvent("group:addMember", p.group.id + " - " + p.el.id);
-    });
-    j.bind("group:removeMember", function(p) {
-        _appendEvent("group:removeMember", p.group.id + " - " + p.el.id);
-    });
-    j.bind("group:expand", function(p) {
-        _appendEvent("group:expand", p.group.id);
-    });
-    j.bind("group:collapse", function(p) {
-        _appendEvent("group:collapse", p.group.id);
-    });
-    j.bind("group:add", function(p) {
-        _appendEvent("group:add", p.group.id);
-    });
-    j.bind("group:remove", function(p) {
-        _appendEvent("group:remove", p.group.id);
-    });
-    // NOTE ordering here. we make one draggable before adding it to the group, and we add the other to the group
-    //before making it draggable. they should both be constrained to the group extents.
-    
-    j.connect({source:c1_1, target:container2 ,  type:"basic"});
-    
     j.addGroup({
         el:container1,
         id:"one",
@@ -56,11 +29,77 @@ jsPlumb.ready(function () {
     j.addToGroup("one", c1_1);
     j.addToGroup("one", c1_2);
     j.addToGroup("one", c1_3);
-
-    j.draggable(c1_1);
-    j.draggable(c1_2);
-    j.draggable(c1_3);
-    
+    var connectorPaintStyle = {
+            strokeWidth: 2,
+            stroke: "#32c24d",
+            joinstyle: "round",
+            outlineStroke: "white",
+            outlineWidth: 2
+        },
+    // .. and this is the hover style.
+        connectorHoverStyle = {
+            strokeWidth: 2,
+            stroke: "#32c24d",
+            outlineWidth: 2,
+            outlineStroke: "white"
+        },
+        endpointHoverStyle = {
+            fill: "#32c24d",
+            stroke: "#32c24d"
+        },
+        sourceEndpoint = {
+                endpoint: "Dot",
+                paintStyle: {
+                    stroke: "#32c24d",
+                    fill: "transparent",
+                    radius: 2,
+                    strokeWidth: 1
+                },
+                isSource: true,
+                connector: [ "StateMachine", { stub: [40, 60], gap: 10, cornerRadius: 5, alwaysRespectStubs: true } ],
+                connectorStyle: connectorPaintStyle,
+                hoverPaintStyle: endpointHoverStyle,
+                connectorHoverStyle: connectorHoverStyle,
+                dragOptions: {},
+                overlays: [
+                    [ "Label", {
+                        location: [0.5, 0.5],
+                        label: "Drag",
+                        cssClass: "endpointSourceLabel",
+                        visible:false
+                    } ]
+                ]
+            },
+            targetEndpoint = {
+                endpoint: "Dot",
+                paintStyle: { fill: "#32c24d", radius: 7 },
+                hoverPaintStyle: endpointHoverStyle,
+                maxConnections: -1,
+                dropOptions: { hoverClass: "hover", activeClass: "active" },
+                isTarget: true,
+                overlays: [
+                    [ "Label", { location: [0.5, -0.5], label: "Drop", cssClass: "endpointTargetLabel", visible:false } ]
+                ]
+            };
+    var sourceAnchors = [
+        [ 0, 1, 0, 1 ],
+        [ 0.25, 1, 0, 1 ],
+        [ 0.5, 1, 0, 1 ],
+        [ 0.75, 1, 0, 1 ],
+        [ 1, 1, 0, 1 ]
+    ];
+    j.makeSource(c1_1, {
+        filter:"a",
+        filterExclude:true,
+        maxConnections: 1,
+        endpoint:[ "Dot", { radius: 1, cssClass:"small-blue" } ],
+        anchor:sourceAnchors
+    });
+    j.makeTarget(container2, {
+        dropOptions: { hoverClass: "hover" },
+        anchor:"Top",
+        endpoint:[ "Dot", { radius: 1, cssClass:"large-green" } ]
+    });
     
     j.addGroup({
         el:container2,
@@ -71,10 +110,7 @@ jsPlumb.ready(function () {
     j.addToGroup("two", c2_1);
     j.addToGroup("two", c2_2);
     j.addToGroup("two", c2_3);
-    
-    j.draggable(c2_1);
-    j.draggable(c2_2);
-    j.draggable(c2_3);
+
 
     // the independent element that demonstrates the fact that it can be dropped onto a group
     j.draggable("standalone");
@@ -82,14 +118,6 @@ jsPlumb.ready(function () {
     j.on(canvas, "click", ".del", function() {
         var g = this.parentNode.getAttribute("group");
         j.removeGroup(g, this.getAttribute("delete-all") != null);
-    });
-
-    // collapse/expand group button
-    j.on(canvas, "click", ".node-collapse", function() {
-        var g = this.parentNode.getAttribute("group"), collapsed = j.hasClass(this.parentNode, "collapsed");
-
-        j[collapsed ? "removeClass" : "addClass"](this.parentNode, "collapsed");
-        j[collapsed ? "expandGroup" : "collapseGroup"](g);
     });
 
     jsPlumb.fire("jsPlumbDemoLoaded", j);
