8e94448007e1d2e6735722e9ba892879e2f0082d
羽丰乐成 <yunweixiang@ukewo.cn>
2018-10-22 08:49:21 +0800

    no commit message

---
+++ src/main/java/com/ukefu/webim/service/repository/QualityMissionRepository.java
@@ -0,0 +1,14 @@
+package com.ukefu.webim.service.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ukefu.webim.web.model.QualityMission;
+
+public abstract interface QualityMissionRepository extends JpaRepository<QualityMission, String>{
+	
+	public Page<QualityMission> findByOrgi(String orgi, Pageable page);
+	
+	public QualityMission findByIdAndOrgi(String id ,String orgi);
+}
