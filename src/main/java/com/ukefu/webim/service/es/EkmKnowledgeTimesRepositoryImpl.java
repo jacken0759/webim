7bcc30ea5fffa86003e1e47b70461c0d6774207e
rico <zhangrucong@ukewo.cn>
2018-10-22 16:37:15 +0800

    no commit message

--- src/main/java/com/ukefu/webim/service/es/EkmKnowledgeTimesRepositoryImpl.java
+++ src/main/java/com/ukefu/webim/service/es/EkmKnowledgeTimesRepositoryImpl.java
@@ -2,12 +2,14 @@ package com.ukefu.webim.service.es;
 
 import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 
+import java.util.Date;
 import java.util.List;
 
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.HasParentQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.sort.FieldSortBuilder;
 import org.elasticsearch.search.sort.SortOrder;
@@ -111,7 +113,14 @@ public class EkmKnowledgeTimesRepositoryImpl implements EkmKnowledgeTimesESRepos
 		}
 		boolQueryBuilder.must(bq) ;
 		boolQueryBuilder.must(QueryBuilders.termQuery("orgi", orgi)) ;
-		HasParentQueryBuilder hasParentQueryBuilder=QueryBuilders.hasParentQuery("uk_ekm_kb_master",QueryBuilders.termQuery("datastatus", false));
+		//过滤掉已过期的知识
+		QueryBuilder beginFilter = QueryBuilders.boolQuery().should(QueryBuilders.missingQuery("begintime")).should(QueryBuilders.rangeQuery("begintime").to(new Date().getTime())) ;
+		QueryBuilder endFilter = QueryBuilders.boolQuery().should(QueryBuilders.missingQuery("endtime")).should(QueryBuilders.rangeQuery("endtime").from(new Date().getTime())) ;
+		BoolQueryBuilder parentbq = QueryBuilders.boolQuery();
+		parentbq.must(beginFilter) ;
+		parentbq.must(endFilter) ;
+		parentbq.must(QueryBuilders.termQuery("datastatus", false)) ;
+		HasParentQueryBuilder hasParentQueryBuilder=QueryBuilders.hasParentQuery("uk_ekm_kb_master",parentbq);
 		boolQueryBuilder.must(hasParentQueryBuilder) ;
 		NativeSearchQueryBuilder searchQueryBuilder = new NativeSearchQueryBuilder().withQuery(boolQueryBuilder).withSort(new FieldSortBuilder("viewtimes").unmappedType("long").order(SortOrder.DESC));
 		searchQueryBuilder.withPageable(pageable) ;
