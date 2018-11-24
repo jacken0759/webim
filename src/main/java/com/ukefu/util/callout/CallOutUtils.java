5f05808eea3dd21c38270847110dba49894931b6
贝密 <open@ukewo.cn>
2018-11-24 17:07:19 +0800

    1、增加电销关注点

--- src/main/java/com/ukefu/util/callout/CallOutUtils.java
+++ src/main/java/com/ukefu/util/callout/CallOutUtils.java
@@ -320,7 +320,7 @@ public class CallOutUtils {
 		
 		metaDataTable.getTableproperty().add(initProperties("levelscore", "评分", "Long", event.getOrgi() , event.getTablename() , true)) ;
 		
-		metaDataTable.getTableproperty().add(initProperties("focustimes", "关注点", "Long", event.getOrgi() , event.getTablename() , true)) ;
+		metaDataTable.getTableproperty().add(initProperties("focustimes", "关注点次数", "Long", event.getOrgi() , event.getTablename() , true)) ;
 	}
 	
 	public static TableProperties initProperties(String name ,String title, String type ,String orgi ,String tableName , boolean sysfield) {
