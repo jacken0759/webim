eb358e364295dc45f63864ccf5edac5d62c278c1
贝密 <open@ukewo.cn>
2018-12-08 18:35:37 +0800

    1、增加独立上传接口

--- src/main/java/com/ukefu/webim/web/handler/api/rest/ApiIMController.java
+++ src/main/java/com/ukefu/webim/web/handler/api/rest/ApiIMController.java
@@ -1,5 +1,6 @@
 package com.ukefu.webim.web.handler.api.rest;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
@@ -11,15 +12,20 @@ import javax.validation.Valid;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ukefu.core.UKDataContext;
 import com.ukefu.util.Menu;
@@ -30,6 +36,7 @@ import com.ukefu.webim.service.repository.AgentServiceSatisRepository;
 import com.ukefu.webim.service.repository.ChatMessageRepository;
 import com.ukefu.webim.service.repository.ConsultInviteRepository;
 import com.ukefu.webim.service.repository.LeaveMsgRepository;
+import com.ukefu.webim.util.MessageUtils;
 import com.ukefu.webim.util.OnlineUserUtils;
 import com.ukefu.webim.util.RestResult;
 import com.ukefu.webim.util.RestResultType;
@@ -40,6 +47,7 @@ import com.ukefu.webim.web.model.AiConfig;
 import com.ukefu.webim.web.model.LeaveMsg;
 import com.ukefu.webim.web.model.SystemConfig;
 import com.ukefu.webim.web.model.UKeFuDic;
+import com.ukefu.webim.web.model.UploadStatus;
 
 import freemarker.template.TemplateException;
 import io.swagger.annotations.Api;
@@ -62,6 +70,9 @@ public class ApiIMController extends Handler{
 
 	@Autowired
 	private LeaveMsgRepository leaveMsgRes ;
+	
+	@Value("${web.upload-path}")
+    private String path;
 	
 	/**
 	 * 返回在线网站配置
@@ -291,4 +302,51 @@ public class ApiIMController extends Handler{
     public ResponseEntity<RestResult> mobilesuggest(ModelMap map , HttpServletRequest request , HttpServletResponse response,  @PathVariable String appid ,@Valid String q ,@Valid String traceid,@Valid String aiid ,@Valid String p ,@Valid String exchange, @Valid String title ,@Valid String url, @Valid String skill, @Valid String id , @Valid String userid , @Valid String agent , @Valid String name , @Valid String email ,@Valid String phone,@Valid String ai,@Valid String orgi ,@Valid String product,@Valid String description,@Valid String imgurl,@Valid String pid,@Valid String purl) throws Exception {
     	return new ResponseEntity<>(new RestResult(RestResultType.OK ,!StringUtils.isBlank(appid) && !StringUtils.isBlank(q) ? OnlineUserUtils.suggest(q, orgi, userid,OnlineUserUtils.cousult(appid ,null, consultInviteRepository) , aiid , skill  ): null), HttpStatus.OK);
     }
+	
+	
+	@RequestMapping("/image/upload")
+    @Menu(type = "im" , subtype = "image" , access = true)
+    public ModelAndView upload(ModelMap map,HttpServletRequest request , @RequestParam(value = "imgFile", required = false) MultipartFile imgFile , @Valid String channel, @Valid String userid, @Valid String username , @Valid String appid , @Valid String orgi, @Valid String paste) throws IOException {
+    	ModelAndView view = request(super.createRequestPageTempletResponse("/apps/im/upload")) ; 
+    	UploadStatus upload = null ;
+    	String fileName = null ;
+    	if(imgFile!=null && imgFile.getOriginalFilename().lastIndexOf(".") > 0 && !StringUtils.isBlank(userid)){
+    		File uploadDir = new File(path , "upload");
+    		if(!uploadDir.exists()){
+    			uploadDir.mkdirs() ;
+    		}
+    		String fileid = UKTools.md5(imgFile.getBytes()) ;
+    		if(imgFile.getContentType()!=null && imgFile.getContentType().indexOf("image") >= 0){
+	    		fileName = "upload/"+fileid+"_original" ;
+	    		File imageFile = new File(path , fileName) ;
+	    		FileCopyUtils.copy(imgFile.getBytes(), imageFile);
+	    		String thumbnailsFileName = "upload/"+fileid ;
+	    		UKTools.processImage(new File(path , thumbnailsFileName), imageFile) ;
+	    		
+	    		
+	    		upload = new UploadStatus("0" , "/res/image.html?id="+thumbnailsFileName);
+	    		
+	    		String image =  "/res/image.html?id="+thumbnailsFileName ;
+	    		if(request.getServerPort() == 80){
+	    			image = "/res/image.html?id="+thumbnailsFileName;
+				}else{
+					image = "/res/image.html?id="+thumbnailsFileName;
+				}
+	    		if(paste == null){
+	    			if(!StringUtils.isBlank(channel)){
+		    			MessageUtils.uploadImage(image , fileid ,(int)imgFile.getSize() , imgFile.getName() , channel, userid , username , appid , orgi);
+		    		}else{
+		    			MessageUtils.uploadImage(image , fileid ,(int)imgFile.getSize() , imgFile.getName() , userid);
+		    		}
+	    		}
+    		}else{
+    			upload = new UploadStatus("请选择文件");
+    		}
+    	}else{
+    		upload = new UploadStatus("请选择文件");
+    	}
+    	map.addAttribute("upload", upload) ;
+        return view ; 
+    }
+    
 }
